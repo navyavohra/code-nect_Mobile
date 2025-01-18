6040252c425d4c27debac660e2aecf6f11782a74
Navya <vohrana@sheridancollege.ca>
2025-01-18 15:28:15 -0500

    added Color assets

---
+++ code-nect/ViewModels/CreateJoinViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  CreateJoinViewModel.swift
+//  code-nect
+//
+//  Created by Navya Vohra on 2025-01-18.
+//
+
+import Foundation
+
+class CreateJoinViewModel: ObservableObject {
+    @Published var channelCode = ""
+    
+    func createChannel() {
+        // Logic for creating a channel
+    }
+    
+    func joinChannel() {
+        // Logic for joining a channel
+    }
+}
