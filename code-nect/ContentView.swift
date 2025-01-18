306edf4ee21c58a7816ab76046aca628fdb83292
Navya <vohrana@sheridancollege.ca>
2025-01-18 15:33:45 -0500

    Updated ContentView

--- code-nect/ContentView.swift
+++ code-nect/ContentView.swift
@@ -5,19 +5,38 @@
 //  Created by Navya Vohra on 2025-01-18.
 //
 
+import SwiftUICore
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Spacer()
+                
+                Text("codenect")
+                    .font(.system(size: 64, weight: .bold))
+                    .foregroundColor(.black)
+                
+                Spacer()
+                
+                NavigationLink(destination: CreateJoinView()) {
+                    Text("<start>")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.primaryPink)
+                        .cornerRadius(10)
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .background(Color.backgroundLight.ignoresSafeArea())
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
