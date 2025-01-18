98088ffb5fdb090b783cb42aa6cc8ecf41df4193
Navya <vohrana@sheridancollege.ca>
2025-01-18 12:45:27 -0500

    Initial Commit

---
+++ code-nect/code_nectApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_nectApp.swift
+//  code-nect
+//
+//  Created by Navya Vohra on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct code_nectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ code-nectTests/code_nectTests.swift
@@ -0,0 +1,17 @@
+//
+//  code_nectTests.swift
+//  code-nectTests
+//
+//  Created by Navya Vohra on 2025-01-18.
+//
+
+import Testing
+@testable import code_nect
+
+struct code_nectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
